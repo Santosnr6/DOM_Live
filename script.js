490b143a1d401a146b1f630fcdb1bdff89117663
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-01-17 14:54:15 +0100

    lösningsförslag sinus tillagt längst ner

--- script.js
+++ script.js
@@ -54,3 +54,141 @@ pNodeRef.addEventListener('mouseleave', (event) => {
     console.log(event.target.textContent);
     event.target.classList.toggle('t-red');
 });
+
+//Lösningsförslag Sinus MiniShop
+
+// Code here!
+/* 1 */
+const refOne = document.querySelector('.art-1 h3');
+refOne.textContent = 'Potato';
+
+/* 2 */
+const refTwo = document.querySelector('nav a');
+refTwo.textContent = 'Start';
+
+/* 3 */
+const refThree = document.querySelector('nav a:nth-child(3)');
+refThree.textContent = 'Mail us';
+
+/*
+let linkRefs = document.querySelectorAll('nav a');
+for(let i = 0; i < linkRefs.length; i++) {
+    if(i === linkRefs.length - 1) 
+        linkRefs[i].textContent = 'Mail us';
+}
+*/
+
+/* 4 */
+const refFour = document.querySelector('.art-2 p');
+refFour.textContent = 'En lagom tjock tröja i sina bästa år!';
+
+/* 5 */
+const refFive = document.querySelector('.art-3 button');
+refFive.style.backgroundColor = 'red';
+refFive.textContent = 'Hjälp';
+
+/* 6 */
+const refSix = document.querySelector('.art-2');
+refSix.style.backgroundColor = 'pink';
+
+/* 7 */
+const refSeven = document.querySelector('section article:nth-child(2)');
+const address = refSeven.querySelector('p');
+
+address.innerHTML = 'Gamla Vägen 115 <br> 664 34 Grums';
+
+/* 8 */
+const refsEight = document.querySelectorAll('p');
+refsEight.forEach(p => {
+    p.style.color = 'red';
+});
+
+/* 9 */
+const refsBtn = document.querySelectorAll('button');
+for(let i = 0; i < refsBtn.length; i++) {
+    refsBtn[i].textContent = 'Add to cart';
+}
+
+/* 10 */
+refTwo.classList.add('active');
+
+/* 11 */
+const refEleven = document.querySelector('.logo');
+//refEleven.classList.remove('logo');
+
+/* 12 */
+const navRef = document.querySelector('nav');
+const imgRef1 = document.querySelector('nav img');
+const linkRef = document.createElement('a');
+linkRef.textContent = 'Checkout';
+linkRef.href = '#';
+
+navRef.insertBefore(linkRef, imgRef1);
+
+/* 13 */
+const articleRef = document.createElement('article');
+const figureRef = document.createElement('figure');
+
+const imgRef = document.createElement('img');
+imgRef.src = 'img/hoodie-forrest.png';
+imgRef.alt = 'hoodie';
+figureRef.appendChild(imgRef);
+
+const headerRef = document.createElement('h2');
+headerRef.textContent = 'Sinus Hoodie'
+
+const subheaderRef = document.createElement('h3');
+subheaderRef.textContent = 'Forrest';
+
+const pRef = document.createElement('p');
+pRef.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolores.';
+
+const btnRef = document.createElement('button');
+btnRef.textContent = 'Add to cart';
+
+articleRef.appendChild(figureRef);
+articleRef.appendChild(headerRef);
+articleRef.appendChild(subheaderRef);
+articleRef.appendChild(pRef);
+articleRef.appendChild(btnRef);
+
+// let articleHTML = `
+//     <figure><img src="img/hoodie-forrest.png" alt="hoodie"></figure>
+//     <h2>Sinus Hoodie</h2>
+//     <h3>Forrest</h3>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolores.</p>
+//     <button>Add to cart</button>
+// `;
+
+// articleRef.innerHTML = articleHTML;
+
+document.querySelector('main').appendChild(articleRef);
+
+
+/*
+<article class="art-1">
+    <figure><img src="img/hoodie-ash.png" alt="hoodie"></figure>
+    <h2>Sinus Hoodie</h2>
+    <h3>Ash</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloremque ducimus enim!</p>
+    <button>buy</button>
+</article>
+*/
+
+/* 14 */
+const logoRef = document.querySelector('.logo');
+
+logoRef.addEventListener('click', () => {
+    console.log('found you!');
+});
+
+/* 15 */
+const articleRefs = document.querySelectorAll('main article');
+
+articleRefs.forEach(article => {
+    article.addEventListener('click', () => {
+        const textRef = article.querySelector('h3');
+        console.log('Hi! Im article ' + textRef.textContent);
+    });
+});
+
